70ee84048021298d51bc821f094048e7610ebdcd
DANRAMAGE <ChiefDan@gmail.com>
2025-10-21 16:55:03 -0400

    Reorganize package structure.

--- src/datautilities/data_record.py
+++ src/datautilities/data_record.py
@@ -1,10 +1,18 @@
 class DataRecord:
     def __init__(
-        self, organization, platform_handle, obs_type, uom_type, value, date_time
+        self,
+        organization,
+        platform_handle,
+        obs_type,
+        uom_type,
+        s_order,
+        value,
+        date_time,
     ):
         self.organization = organization
         self.platform_handle = platform_handle
         self.obs_type = obs_type
         self.uom_type = uom_type
+        self.s_order = s_order
         self.value = value
         self.date_time = date_time
